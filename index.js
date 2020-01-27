d97ef29aa3c530dda031d87c1b6281ad92a175f4
github username <github email address>
2020-01-27 10:55:51 -0500

    pause

---
+++ index.js
@@ -0,0 +1,27 @@
+function wakeDog() {
+    console.log("Wake Byron the poodle");
+}
+
+function leashDog() {
+    console.log("Leash Byron the poodle");
+}
+
+function walkToPark() {
+    console.log("Walk to the park with ${`dogBreed`} the ${`dogBreed`}};
+}
+
+function throwFrisbee() {
+    console.log("Throw the frisbee for Byron the poodle");
+}
+
+function walkHome() {
+    console.log("Walk home with Byron the poodle");
+}
+
+function unleashDog() {
+    console.log("Unleash Byron the poodle");
+}
+
+const routines = [wakeDog(), leashDog(), walkToPark(), throwFrisbee(), walkHome(), unleashDog()];
+
+
