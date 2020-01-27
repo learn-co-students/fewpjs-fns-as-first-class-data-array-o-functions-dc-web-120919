3fb0bc684aef1740438d5c9e0c6f8771a8c2f14d
github username <github email address>
2020-01-27 11:06:28 -0500

    pause

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-function wakeDog() {
-    console.log("Wake Byron the poodle");
+var wakeDog = function(dogName="Byron", dogBreed="poodle") {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`
+  }
+
+var leashDog = function(dogName="Byron", dogBreed="poodle") { 
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`
 }
 
-function leashDog() {
-    console.log("Leash Byron the poodle");
+var walkToPark = function(dogName="Byron", dogBreed="poodle") { console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+return `Walk to the park with ${dogName} the ${theBreed}`
 }
 
-function walkToPark() {
-    console.log("Walk to the park with ${`dogBreed`} the ${`dogBreed`}};
+var throwFrisbee = function(dogName="Byron", dogBreed="poodle"){
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
-function throwFrisbee() {
-    console.log("Throw the frisbee for Byron the poodle");
+var walkHome = function(dogName="Byron", dogBreed="poodle") {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`
 }
 
-function walkHome() {
-    console.log("Walk home with Byron the poodle");
-}
-
-function unleashDog() {
-    console.log("Unleash Byron the poodle");
+var unleashDog = function(dogName="Byron", dogBreed="poodle") { console.log(`Unleash ${dogName} the ${dogBreed}`);
+return `Unleash ${dogName} the ${dogBreed}`
 }
 
 const routines = [wakeDog(), leashDog(), walkToPark(), throwFrisbee(), walkHome(), unleashDog()];
